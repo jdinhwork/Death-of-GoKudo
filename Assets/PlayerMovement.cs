32464d3f8e8b340752c1fb950a8a324ab057b4b0
Jinaito <48036744+Jinaito@users.noreply.github.com>
2019-03-06 03:21:28 -0600

    Player Animations
    - Added jump animation
    - Added idle animation
    - Added run animation
    - Increased size of player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -16,12 +17,19 @@ public class PlayerMovement : MonoBehaviour
         // Get input
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
     }
 
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
     void FixedUpdate()
     {
         // Move our character
