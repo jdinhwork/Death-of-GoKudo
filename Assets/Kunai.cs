2d137452830f4d168e9ec84c293ad32683e9dbb3
Jinaito <48036744+Jinaito@users.noreply.github.com>
2019-03-06 23:03:22 -0600

    Throw Animation
    - Implemented throw animation
    - Added kunai prefab
    - Added kunai script
    - Added very basic enemy script

---
+++ Assets/Kunai.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kunai : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Animator animator;
+
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask notToHit;
+
+    float timeToFire = 0;
+    public Transform firePoint;
+    public GameObject kunaiPrefab;
+
+    // Start is called before the first frame update
+    void Awake ()
+    {
+        firePoint = transform.FindChild ("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No FirePoint?");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown ("Fire1"))
+            {
+                Shoot();
+                animator.SetBool("IsThrowing", true);
+            }
+            else
+            {
+                animator.SetBool("IsThrowing", false);
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown ("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+    
+    void Shoot ()
+    {
+        Instantiate(kunaiPrefab, firePoint.position, firePoint.rotation);
+    }
+}
