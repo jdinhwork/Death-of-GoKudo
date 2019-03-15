959e06f5bb3e91e21e59e651496a626eaea16a92
Jinaito <48036744+Jinaito@users.noreply.github.com>
2019-03-14 20:29:42 -0500

    Rope Mechanic
    - Added Rope Mechanic
       (Right-Click to Hook and Detach)

---
+++ Assets/Scripts/ThrowHook.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowHook : MonoBehaviour
+{
+
+
+    public GameObject hook;
+
+
+    public bool ropeActive;
+
+    GameObject curHook;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+
+            if (ropeActive == false)
+            {
+                Vector2 destiny = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+                curHook = (GameObject)Instantiate(hook, transform.position, Quaternion.identity);
+
+                curHook.GetComponent<RopeScript>().destiny = destiny;
+
+
+                ropeActive = true;
+            }
+            else
+            {
+
+                //delete rope
+
+                Destroy(curHook);
+
+
+                ropeActive = false;
+
+            }
+        }
+
+
+    }
+}
