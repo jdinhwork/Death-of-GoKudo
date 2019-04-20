6c848b5f6c91eac86a2e92c9f513689304739809
KratosSM <48036871+KratosSM@users.noreply.github.com>
2019-04-19 21:40:13 -0500

    enemy
    added enemy with an attack

---
+++ Assets/EnemyAttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public Animator animator;
+
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask notToHit;
+
+    float timeToFire = 0;
+    public Transform firePoint;
+    public GameObject kunaiPrefab;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No FirePoint?");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void Shoot()
+    {
+        if (fireRate == 0)
+        {
+
+        //    animator.SetBool("IsThrowing", true);
+        }
+       
+        
+
+
+
+        Instantiate(kunaiPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    public void DisableAnimatorBool()
+    {
+       // animator.SetBool("IsThrowing", true);
+    }
+
+}
---
+++ Assets/MoveEnemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    Rigidbody2D Rb;
+    Vector3 Groundcheck;
+    public float speed;
+    private bool IsMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsMoving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsMoving)
+        {
+            Move();
+        }
+       
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.left * speed, Space.Self); 
+    }
+
+    public void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void SetIsMoving(bool b)
+    {
+        IsMoving = b;
+    }
+
+    public void Attack()
+    {
+        GetComponent<EnemyAttack>().Shoot();
+
+    }
+
+
+}
